eec639dc4b541c79f3c70fb341b24ac6e0eef8f1
clem9669 <user@localhost.localdomain>
2020-11-25 16:52:34 +0100

    Updating

--- gen.py
+++ gen.py
@@ -3,25 +3,37 @@
 import string
 
 # print(string.digits)
-a = """!"#$%&'()*+,-./:;<=>?@[\]^_{|}~"""
+a = """!"#$%&'()*+,-./:;<=>?@[\]^_{|}~1234567890"""
 
 # for i in a:
 # 	print("$"+i)
 
-for i in string.digits:
-	for j in a:
+# for i in string.digits:
+# 	for j in a:
+# 		print("$"+j+" $"+i)
+
+# for k in string.digits:
+# 	for i in string.digits:
+# 		for j in a:
+# 			print("$"+j+" $"+i+" $"+k)
+
+# for i in string.digits:
+# 	for j in a:
+# 		print("c $"+j+" $"+i)
+
+# for k in string.digits:
+# 	for i in string.digits:
+# 		for j in a:
+# 			print("c " +"$"+j+" $"+i+" $"+k)
+
+for j in string.ascii_letters:
+	for i in a:
 		print("$"+j+" $"+i)
 
-for k in string.digits:
-	for i in string.digits:
-		for j in a:
-			print("$"+j+" $"+i+" $"+k)
-
-for i in string.digits:
-	for j in a:
+for j in string.ascii_letters:
+	for i in a:
 		print("c $"+j+" $"+i)
 
-for k in string.digits:
-	for i in string.digits:
-		for j in a:
-			print("c " +"$"+j+" $"+i+" $"+k)
+for j in string.ascii_letters:
+	for i in a:
+		print("u $"+j+" $"+i)

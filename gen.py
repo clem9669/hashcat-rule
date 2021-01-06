039fd4edbaf131ac10a968d31544fab1b62bda13
clem9669 <user@localhost.localdomain>
2021-01-06 15:33:53 +0100

    Adding more suffix punctuation + 3 digits

--- gen.py
+++ gen.py
@@ -9,9 +9,9 @@ b = """!"#$ %&'()*+,-./:;<=>?@[\]^_{|}~"""
 toto = "1234567890"
 
 
-for i in range(1900,2200):
+for i in range(0000,1000):
 	for j in b:
-		print(str(j)+str(i))
+		print("$"+str(j)+" $"+str(i).zfill(3))
 
 # for i in toto:
 # 	for j in toto:
